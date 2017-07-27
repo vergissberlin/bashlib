c5c582cf2a7aa2b514fdd992b374c68060ba81f3
André Lademann <vergissberlin@googlemail.com>
2017-07-27 23:59:08 +0200

    Do not use parameter yet.

--- ci/scripts/run.sh
+++ ci/scripts/run.sh
@@ -9,11 +9,9 @@ figlet -f digital "Run tests"
 echo
 echo
 echo "$(uname -a)"
-echo "Try to run tests on: ${DISTRIBUTION}"
 echo
 [ -e /etc/lsb-release ] && cat /etc/lsb-release
 echo
-
 
 cd ./resource-bashlib/
 sh ./test/suite.sh
